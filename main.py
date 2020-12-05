e9c215210bd396a604f4918df20171f0d2ef8b56
Your Name <you@example.com>
2020-12-05 14:37:26 +0100

    további haladás

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def set_table():
         map.append(sor)
 
     print("Üdvözöllek a 2048-as játékban, a aprancsok a következők: ")
-    print("fel/le/balra/jobbra/end")
+    print("w-fel/a-balra/s-le/d-jobbra/end-kimentés,befejezés")
     print("a játék kimenti ahol állsz ha az end parancsot nyomod meg, sok szerencsét")
     
     return map
@@ -47,14 +47,78 @@ def lose(map):
     else:
         return False
 
+def összehúzás(mat): 
+   
+    változás = False
+    new_map = [] 
+
+    for i in range(4): 
+        new_mat.append([0] * 4) 
+
+    for i in range(4): 
+        pos = 0
+  
+        for j in range(4): 
+            if(mat[i][j] != 0): 
+                
+                new_mat[i][pos] = mat[i][j] 
+                  
+                if(j != pos): 
+                    changed = True
+                pos += 1
+    return new_map, változás
+
 def összegez(map):
-    újmap = []
+    változás = False
     for i in range(len(map)):
         for j in range(len(map[i])):
-            if map[i][j] == map[i][]
+            if (map[i][j] == 0 and map[i][j + 1] == 0) and map[i][j] != 0:
+                map[i][j] = map[i][j] *2
+                map[i][j+1] = 0
+                változott = True
+    
+    return map, változás
+
+def sorváltás(map):
+    new_map = []
+    for i in range(len(4)):
+        new_map.append([])
+        for j in range(len(4)):
+            new_map[i].append(map[i][3-j])
+
+    return new_map
+
+def mapfordítás(map):
+    new_map = []
+    for i in range(4):
+        new_map.append([])
+        for j in range(4):
+            new_map[i].append(map[j][i])
+    
+    return new_map
+
+def mentés(map):
+    import json
+    file = open("állás.txt","wt")
+    json.dump(map, file)
+    file.close()
+
+def betöltés(map):
+    import json
+    file = open("állás.txt","rt")
+    map = json.load(file)
+    file.close()
+    return map
+
+def getdirection():
+    direction = input("Adj meg egy irányt! ")
+    return direction
+
+###főprogram###
+térkép = set_table()
+while True:
+    tékép = give_new_2(map)
+    direction = getdirection()
+    if direction == "a":
 
     
-###főprogram###
-
-térkép = set_table()
-térkép = give_new_2(map)
