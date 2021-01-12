cedfb7192dcf4a0a4e8e87924e7837b659a4e3e9
Your Name <you@example.com>
2021-01-12 09:22:03 +0100

    Minden kész, és updatelve

--- alapok.py
+++ alapok.py
@@ -1,28 +1,34 @@
 from typing import List, Set, Dict, Tuple, Optional
 import json
 import random
+listexample1 = [[2,2,2,2],[2,2,2,2],[0,0,0,0],[0,0,0,0]]
+listexample2 = [[2,2,2,2],[16,16,16,16],[4,4,4,4],[2,0,2,2]]
+listexample3 = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,2048]]
 
 def mapprint(map:list) -> None:
     """The function that prints out the map
 
     Args:
         map (list): The map that is given here will be printed out
+
+    >>> mapprint(listexample1)
+    [2, 2, 2, 2]
+    [2, 2, 2, 2]
+    [0, 0, 0, 0]
+    [0, 0, 0, 0]
+    >>> mapprint(listexapmle2)
+    [2, 2, 2, 2]
+    [16, 16, 16, 16]
+    [4, 4, 4, 4]
+    [2, 2, 2, 2]
+    >>> mapprint(listexample3)
+    [8, 8, 8, 8]
+    [8, 8, 8, 8]
+    [0, 0, 0, 0]
+    [2, 2, 2, 2048]
     """
     for i in range(4):
         print(map[i])
-
-#test
-
-#lista = [[2,2,2,2],[2,2,2,2],[0,0,0,0],[0,0,0,0]]
-#lista2 = [[2,2,2,2],[16,16,16,16],[4,4,4,4],[2,2,2,2]]
-#lista3 = [[2,2,2,2],[2,2,2,2],[0,0,0,0],[0,0,0,0]]
-#lista4 = [[0,0,0,0],[2,2,2,2],[2,2,2,2],[0,0,0,0]]
-#lista5 = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,2048]]
-#print(mapprint(lista))
-#print(mapprint(lista2))
-#print(mapprint(lista3))
-#print(mapprint(lista4))
-#print(mapprint(lista5))
 
 
 def give_new_2(map:list) -> None:
@@ -30,48 +36,35 @@ def give_new_2(map:list) -> None:
 
     Args:
         map (list): The map that u want to use
+    >>> give_new_2(listexample1)
+    [[2, 2, 2, 2], [2, 2, 2, 2], [0, 0, 0, 0], [2, 0, 0, 0]]
+    >>> give_new_2(listexample2)
+    [[2, 2, 2, 2], [16, 16, 16, 16], [4, 4, 4, 4], [2, 2, 2, 2]]
     """
     sor = random.randint(0,3)
     oszlop = random.randint(0,3)
-    if map[sor][oszlop] == 0:
-        map[sor][oszlop] = 2
-    else:
-        while map[sor][oszlop] != 0:
-            sor = random.randint(0,3)
-            oszlop = random.randint(0,3)
-        map[sor][oszlop] = 2
-#test
-#lista = [[2,2,2,2],[2,2,2,2],[0,0,0,0],[0,0,0,0]]
-#lista2 = [[0,0,0,0],[2,2,2,2],[2,2,2,2],[0,0,0,0]]
-#lista3 = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,2048]]
-#give_new_2(lista)
-#give_new_2(lista2)
-#give_new_2(lista3)
-#print(lista)
-#print(lista2)
-#print(lista3)
+    while map[sor][oszlop] != 0:
+        sor = random.randint(0,3)
+        oszlop = random.randint(0,3)
+    map[sor][oszlop] = 2
 
-
-def set_table():
+def set_table() -> None:
     """This is the start of everything, this will generate the first table, with a 2 in a random  place
 
     Returns:
         None: This returns nothing, only prints out the table
+    
+    >>> set_table()
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 2, 0]]
     """
     map = []
     for i in range(4):
         sor = [0]*4
         map.append(sor)
 
-    print("Üdvözöllek a 2048-as játékban, a aprancsok a következők: ")
-    print("w-fel/a-balra/s-le/d-jobbra/save-kimentés,befejezés")
-    print("a játék kimenti ahol állsz ha az end parancsot nyomod meg, sok szerencsét")
-    print("Add meg a parncsot")
     give_new_2(map)
 
     return map
-#test
-#print(set_table())
 
 def win(map:list) -> bool:
     """The function that will check if you have won yet or not
@@ -81,6 +74,12 @@ def win(map:list) -> bool:
 
     Returns:
         bool: Will give a true if you have won and a false if you haven't won'
+    >>> win(listexample1)
+    False
+    >>> win(listexample2)
+    False
+    >>> win(listexample3)
+    True
     """
     for i in range(4):
         for j in range(4):
@@ -88,22 +87,23 @@ def win(map:list) -> bool:
                 return True
     
     return False
-#test
-
-#lista = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,2048]] -> True
-#lista2 = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,0]] -> False
-#print(win(lista))
-#print(win(lista2))
 
 
-def horizontal_move_exists(map:list) -> bool:
-    """This is used in the lose function and checks if you can move horizontally
+def can_it_merge_horizontally(map:list) -> bool:
+    """This is used in the lose function and checks if you can merge horizontally or not
 
     Args:
         map (list): it checks in this map
 
     Returns:
         bool: Return a True if you can, a false if you can't
+    
+    >>> can_it_merge_horizontally(listexample1)
+    True
+    >>> can_it_merge_horizontally(listexample2)
+    True
+    >>> can_it_merge_horizontally(listexample3)
+    True
     """
     for i in range(len(map)):
         for j in range(len(map[i])-1):
@@ -111,19 +111,22 @@ def horizontal_move_exists(map:list) -> bool:
                 return True
     
     return False
-#test
-#lista = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,2048]]
-#print(horizontal_move_exists(lista))
 
 
-def vertical_move_exists(map:list) -> bool:
-    """This will check if you can move vertically or not, also used in the lose function
+def can_it_merge_vertically(map:list) -> bool:
+    """This function checks whether you can merge vertically or not
 
     Args:
-        map (list): The map u checking in
+        map (list): The list u want to check
 
     Returns:
-        bool: Return a True if you can, a false if you can't
+        bool: Rteurn a true if you can a false otherwise
+    >>> can_it_merge_vertically(listexample1)
+    True
+    >>> can_it_merge_vertically(listexample2)
+    False
+    >>> can_it_merge_vertically(listexample3)
+    True
     """
     for i in range(len(map)-1):
         for j in range(len(map[i])):
@@ -131,10 +134,28 @@ def vertical_move_exists(map:list) -> bool:
                 return True
     
     return False
-#test
-#lista = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,2048]]
-#print(vertical_move_exists(lista))
 
+def is_there_zero(map:list) -> bool:
+    """This function checks if there is a 0 on the map
+
+    Args:
+        map (list): The map u are checking in
+
+    Returns:
+        bool: True if there is a 0 on the map and false otherwise
+    >>> is_there_zero(listexample1)
+    True
+    >>> is_there_zero(listexample2)
+    True
+    >>> is_there_zero(listexample3)
+    True
+    """
+    nulla = 0
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == 0:
+                return True
+    return False
 
 def lose(map:list) -> bool:
     """This function checks if you have lost or not. If there isnt any 0-s and you cant move vertically or horizontally, you have lost
@@ -144,53 +165,36 @@ def lose(map:list) -> bool:
 
     Returns:
         bool: Return a True if you Lost, a false if you didnt'
+    
+    >>> lose(listexample1)
+    False
+    >>> lose(listexample2)
+    False
+    >>> lose(listexample3)
+    False
     """
-    nulla = 0
-    for i in range(len(map)):
-        for j in range(len(map[i])):
-            if map[i][j] == 0:
-                nulla += 1
-    if nulla == 0 and (vertical_move_exists(map) == False and horizontal_move_exists(map) == False):
+    if not is_there_zero(map) and not can_it_merge_vertically(map) and not can_it_merge_horizontally(map):
         return True
     else:
         return False
-#test
-#lista = [[2,4,256,2],[4,8,16,64],[16,32,512,1024],[256,16,4,64]]
-#lista1 = [[8,8,8,8],[8,8,8,8],[8,4,8,4],[2,16,2,2]]
-#lista2 = [[8,8,8,8],[8,8,8,8],[32,64,32,64],[2,8,2,16]]
-#lista3 = [[8,8,8,8],[8,8,8,8],[1024,2,4,16],[2,16,2,16]]
-#lista4 = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,16,2,16]]
-#print(mapprint(lista))
-#print(lose(lista))
-#print(lose(lista1))
-#print(lose(lista2))
-#print(lose(lista3))
-#print(lose(lista4))
 
-def vanenulla(map:list) -> bool:
-    """This function checks if there is a 0 on the map
 
-    Args:
-        map (list): The map u are checking in
 
-    Returns:
-        bool: True if there is a 0 on the map and false otherwise
-    """
-    nulla = 0
-    for i in range(len(map)):
-        for j in range(len(map[i])):
-            if map[i][j] == 0:
-                return True
-    return False
-
-def osszehuzas(map:list) -> list:
-    """This function will pull the numbers in the map together
+def pull_together(map:list) -> list:
+    """This function will reutrn a new map, that is basically the same, but has no zeros inbetween the numbers
 
     Args:
         map (list): The map u are using
 
     Returns:
         list: The pulled form of the map
+    
+    >>> pull_together(listexample1)
+    [[2, 2, 2, 2], [2, 2, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0]]
+    >>> pull_together(listexample2)
+    [[2, 2, 2, 2], [16, 16, 16, 16], [4, 4, 4, 4], [2, 2, 2, 0]]
+    >>> pull_together(listexample3)
+    [[8, 8, 8, 8], [8, 8, 8, 8], [0, 0, 0, 0], [2, 2, 2, 2048]]
     """
     new_map = []
 
@@ -205,21 +209,22 @@ def osszehuzas(map:list) -> list:
                 pos += 1
                 
     return new_map
-#test
-#lista = [[8,8,0,8],[8,8,0,8],[0,0,0,0],[2,2,2,0]]
-#lista2 = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(osszehuzas(lista))
-#print(osszehuzas(lista2))
 
 
-def osszegez(map:list) -> list:
-    """It will add the numbers together, and puts a ^on the second part
+def merge(map:list) -> list:
+    """It will add the numbers together always in left direction, and will use other functions to get other directions done
 
     Args:
         map (list): The map u are checking in
 
     Returns:
         list: The added map
+    >>> merge(listexample1)
+    [[4, 0, 4, 0], [4, 0, 4, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    >>> merge(listexample2)
+    [[4, 0, 4, 0], [32, 0, 32, 0], [8, 0, 8, 0], [2, 0, 4, 0]]
+    >>> merge(listexample3)
+    [[16, 0, 16, 0], [16, 0, 16, 0], [0, 0, 0, 0], [4, 0, 2, 2048]]
     """
     for i in range(len(map)):
         for j in range(len(map[i])- 1):
@@ -228,13 +233,9 @@ def osszegez(map:list) -> list:
                 map[i][j + 1] = 0
     
     return map
-#test
-#lista = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(osszegez(lista)), az output:
-#[[2, 0, 4, 0], [4, 0, 0, 2], [0, 0, 0, 0], [0, 0, 0, 0]]
 
 
-def sorvaltas(map:list) -> list:
+def reverse(map:list) -> list:
     """
     This will reverse the map
 
@@ -243,6 +244,12 @@ def sorvaltas(map:list) -> list:
 
     Returns:
         list: The reversed version of map
+    >>> reverse(listexample1)
+    [[2, 2, 2, 2], [2, 2, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0]]
+    >>> reverse(listexample2)
+    [[2, 2, 2, 2], [16, 16, 16, 16], [4, 4, 4, 4], [2, 2, 0, 2]]
+    >>> reverse(listexample3)
+    [[8, 8, 8, 8], [8, 8, 8, 8], [0, 0, 0, 0], [2048, 2, 2, 2]] 
     """
     new_map = []
     for i in range(4):
@@ -251,13 +258,8 @@ def sorvaltas(map:list) -> list:
             new_map[i].append(map[i][3-j])
 
     return new_map
-#test
-#lista = [[8,8,0,8],[8,8,0,8],[0,0,0,0],[2,2,2,0]]
-#lista2 = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(sorvaltas(lista))
-#print(sorvaltas(lista2))
 
-def mapforditas(map:list) -> list:
+def transpose(map:list) -> list:
     """This will interchange the maps coluumns and rows
 
     Args:
@@ -265,6 +267,12 @@ def mapforditas(map:list) -> list:
 
     Returns:
         list: The interchanged version of the map
+    >>> transpose(listexample1)
+    [[2, 2, 0, 0], [2, 2, 0, 0], [2, 2, 0, 0], [2, 2, 0, 0]]
+    >>> transpose(listexample2)
+    [[2, 16, 4, 2], [2, 16, 4, 0], [2, 16, 4, 2], [2, 16, 4, 2]]
+    >>> transpose(listexample3)
+    [[8, 8, 0, 2], [8, 8, 0, 2], [8, 8, 0, 2], [8, 8, 0, 2048]]
     """
     new_map = []
     for i in range(4):
@@ -273,13 +281,8 @@ def mapforditas(map:list) -> list:
             new_map[i].append(map[j][i])
     
     return new_map
-#test
-#lista = [[8,8,0,8],[8,8,0,8],[0,0,0,0],[2,2,2,0]]
-#lista2 = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(mapforditas(lista))
-#print(mapforditas(lista2))
 
-def mentes(map:list, score:int) -> None:
+def save(map:list, score:int) -> None:
     """This will save the map and the score u are currently at
     """
     import json
@@ -291,7 +294,7 @@ def mentes(map:list, score:int) -> None:
     json.dump(data, file)
     file.close()
 
-def betoltes(filename:str) -> list:
+def load(filename:str) -> list:
     """This will load the data that you have saved
 
     Returns:
@@ -316,11 +319,38 @@ def getcommand() -> str:
 
     Returns:
         str: The command
+    >>> getcommand()
+    s
     """
     command = input("Adj meg egy parancsot! ")
     return command
-#Test
-#print(getcommand())
+
+def score(map:list) -> int:
+    """The function that calculates the score that the player has. First it calculates every number that is on the table, than the value gets multiplied by 10
+
+    Args:
+        map (list): The map
+
+    Returns:
+        int: The claculated score
+    """
+    allscore = 0
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            tempscore = map[i][j] * 10
+            allscore += tempscore
+    
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == 1024:
+                allscore += 500
+    
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == 2048:
+                allscore += 1000
+                
+    return allscore
 
 def move_left(map:list) -> list:
     """The function to handle the left moving command. First u pull the map together, than add the numbers, then pull again
@@ -330,16 +360,18 @@ def move_left(map:list) -> list:
 
     Returns:
         list: the changed map
+
+    >>> move_left(listexample1)
+    [[4, 4, 0, 0], [4, 4, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    >>> move_left(listexample2)
+    [[4, 4, 0, 0], [32, 32, 0, 0], [8, 8, 0, 0], [4, 2, 0, 0]]
+    >>> move_left(listexample3)
+    [[16, 16, 0, 0], [16, 16, 0, 0], [0, 0, 0, 0], [4, 2, 2048, 0]]
     """
-    new_map = osszehuzas(map)
-    new_map = osszegez(new_map)
-    new_map = osszehuzas(new_map)
+    new_map = pull_together(map)
+    new_map = merge(new_map)
+    new_map = pull_together(new_map)
     return new_map
-#test
-#lista = [[8,8,0,8],[8,8,0,8],[0,0,0,0],[2,2,2,0]]
-#lista2 = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(move_left(lista))
-#print(move_left(lista2))
 
 
 def move_right(map:list) -> list:
@@ -350,16 +382,18 @@ def move_right(map:list) -> list:
 
     Returns:
         list: The changed version of the map
+
+    >>> move_right(listexample1)
+    [[0, 0, 4, 4], [0, 0, 4, 4], [0, 0, 0, 0], [0, 0, 0, 0]]
+    >>> move_right(listexample2)
+    [[0, 0, 4, 4], [0, 0, 32, 32], [0, 0, 8, 8], [0, 0, 2, 4]]
+    >>> move_right(listexample3)
+    [[0, 0, 16, 16], [0, 0, 16, 16], [0, 0, 0, 0], [0, 2, 4, 2048]]
     """
-    new_map = sorvaltas(map)
+    new_map = reverse(map)
     new_map = move_left(new_map)
-    new_map = sorvaltas(new_map)
+    new_map = reverse(new_map)
     return new_map
-#test
-#lista = [[8,8,0,8],[8,8,0,8],[0,0,0,0],[2,2,2,0]]
-#lista2 = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(move_right(lista))
-#print(move_right(lista2))
 
 
 def move_up(map:list) -> list:
@@ -370,16 +404,18 @@ def move_up(map:list) -> list:
 
     Returns:
         list: The changed map
+
+    >>> move_up(listexample1)
+    [[4, 4, 4, 4], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    >>> move_up(listexample2)
+    [[2, 2, 2, 2], [16, 16, 16, 16], [4, 4, 4, 4], [2, 0, 2, 2]]
+    >>> move_up(listexample3)
+    [[16, 16, 16, 16], [2, 2, 2, 2048], [0, 0, 0, 0], [0, 0, 0, 0]]
     """
-    new_map = mapforditas(map)
+    new_map = transpose(map)
     new_map = move_left(new_map)
-    new_map = mapforditas(new_map)
+    new_map = transpose(new_map)
     return new_map
-#test
-#lista = [[8,8,0,8],[8,8,0,8],[0,0,0,0],[2,2,2,0]]
-#lista2 = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(move_up(lista))
-#print(move_up(lista2))
 
 def move_down(map:list) -> list:
     """The function that handles the move down command, nfirst u interchange the map, than u move right, than u interchange again to get the default map
@@ -389,14 +425,15 @@ def move_down(map:list) -> list:
 
     Returns:
         list: The changed map
+    >>> move_down(listexample1)
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [4, 4, 4, 4]]
+    >>> move_down(listexample2)
+    [[2, 0, 2, 2], [16, 2, 16, 16], [4, 16, 4, 4], [2, 4, 2, 2]]
+    >>> move_down(listexample3)
+    [[0, 0, 0, 0], [0, 0, 0, 0], [16, 16, 16, 16], [2, 2, 2, 2048]]
     """
-    new_map = mapforditas(map)
+    new_map = transpose(map)
     new_map = move_right(new_map)
-    new_map = mapforditas(new_map)
+    new_map = transpose(new_map)
     return new_map
-#test
-#lista = [[8,8,0,8],[8,8,0,8],[0,0,0,0],[2,2,2,0]]
-#lista2 = [[2,0,2,2],[2,2,0,2],[0,0,0,0],[0,0,0,0]]
-#print(move_down(lista))
-#print(move_down(lista2))
 
--- game.py
+++ game.py
@@ -1,145 +1,111 @@
 import json
 import alapok
-import scoring
 import sys
 from typing import List, Set, Dict, Tuple, Optional
+listexample1 = [[2,2,2,2],[2,2,2,2],[0,0,0,0],[0,0,0,0]]
+listexample2 = [[2,2,2,2],[16,16,16,16],[4,4,4,4],[2,0,2,2]]
+listexample3 = [[8,8,8,8],[8,8,8,8],[0,0,0,0],[2,2,2,2048]]
+def give_instructions() -> None:
+    """Gives out the instructions to the player
+    """
+    print("Üdvözöllek a 2048-as játékban, a aprancsok a következők: ")
+    print("w-fel/a-balra/s-le/d-jobbra/store-kimentés,befejezés")
+    print("a játék kimenti ahol állsz ha az end parancsot nyomod meg, sok szerencsét")
+    print("Add meg a parncsot")
 
+def writescore(map:list) -> None:
+    """This will print out the score
+
+    Args:
+        map (list): The map used to calculate the score
+    """
+    print("Your current score is: {}".format(alapok.score(map)))
 
 def start() -> None:
-    """This will generate the map, or saves in the map from allas.txt if it exists.
+    """This will load a new map, or loads in the map from allas.txt if it exists.
 
     Returns:
         None: This justs prints the table
     """
-    map = alapok.betoltes("állás.txt")
+    give_instructions()
+    map = load("allas.txt")
     alapok.mapprint(map)
-    scoring.writescore(map, scoring.score(map))
+    writescore(map)
     return map
 
-def up(map:list) -> list:
-    """The function to move up and handle the state t´where the player is at right now
+def get_current_state(map:list) -> None:
+    """This function will check what happens to the map after a command has been given out. Did the player lose, did the player win, etc...
 
     Args:
-        map (list): The map u want to change and handle
-
-    Returns:
-        list: the changed version of the map
+        map (list): [description]
     """
-    map = alapok.move_up(map)
     if alapok.win(map) == True:
         print("You have Won! Congratulations!")
         sys.exit()
     elif alapok.lose(map) == True:
         print("You have Lost!")
         sys.exit()
-    elif alapok.vanenulla(map) == False and (alapok.vertical_move_exists(map) == True or alapok.horizontal_move_exists(map) == True):
+    elif alapok.is_there_zero(map) == False and (alapok.vertical_move_exists(map) == True or alapok.horizontal_move_exists(map) == True):
         alapok.mapprint(map)
         print("erre nem tudsz már lépni de másik irányba igen")
-        scoring.writescore(map, scoring.score(map))
-
+        writescore(map)
     else:
         alapok.give_new_2(map)
         alapok.mapprint(map)
-        scoring.writescore(map, scoring.score(map))
+        writescore(map)
 
-    return map
+def save(map:list, score:int) -> None:
+    """This will save the map and the score u are currently at
+    """
+    import json
+    file = open("allas.txt","w")
+    data = {
+        "map": map,
+        "score": score,
+    }
+    json.dump(data, file)
+    file.close()
 
-def down(map:list) -> list:
-    """The function that moves down the map and gets the state of the game
-
-    Args:
-        map (list): the map u want to investigate
+def load(filename:str) -> list:
+    """This will load the data that you have saved
 
     Returns:
-        list: The changed map
+        list: the saved list
     """
-    map = alapok.move_down(map)
-    if alapok.win(map) == True:
-        print("You have Won! Congratulations!")
-        sys.exit()
-    elif alapok.lose(map) == True:
-        print("You have Lost!")
-        sys.exit()
-    elif alapok.vanenulla(map) == False and (alapok.vertical_move_exists(map) == True or alapok.horizontal_move_exists(map) == True):
-        alapok.mapprint(map)
-        print("erre nem tudsz már lépni de másik irányba igen")
-        scoring.writescore(map, scoring.score(map))
-    else:
-        alapok.give_new_2(map)
-        alapok.mapprint(map)
-        scoring.writescore(map, scoring.score(map))
-
+    import json
+    try:
+        file = open(filename,"r")
+        data = json.load(file)
+        global map
+        global score
+        map = data["map"]
+        score = data["score"]
+        file.close()
+    except:
+        map = alapok.set_table()
+        score = 0
     return map
 
-def left(map:list) -> list:
-    """The map that handles the move down command, and gets the state of the game
-
-    Args:
-        map (list): The map u want to change
+def getcommand() -> str:
+    """The function to get the command
 
     Returns:
-        list: ´The changed map
+        str: The command
+    >>> getcommand()
+    s
     """
-    map = alapok.move_left(map)
-    if alapok.win(map) == True:
-        print("You have Won! Congratulations!")
-        sys.exit()
-    elif alapok.lose(map) == True:
-        print("You have Lost!")
-        sys.exit()
-    elif alapok.vanenulla(map) == False and (alapok.vertical_move_exists(map) == True or alapok.horizontal_move_exists(map) == True):
-        alapok.mapprint(map)
-        print("erre nem tudsz már lépni de másik irányba igen")
-        scoring.writescore(map, scoring.score(map))
-    else:
-        alapok.give_new_2(map)
-        alapok.mapprint(map)
-        scoring.writescore(map, scoring.score(map))
-    return map
+    command = input("Adj meg egy parancsot! ")
+    return command
 
-def right(map:list) -> list:
-    """The function that handles the move right command and gets the state of the game
-
-    Args:
-        map (list): The map u want to change
-
-    Returns:
-        list: The changed state of the game
-    """
-    map = alapok.move_right(map)
-    if alapok.win(map) == True:
-        print("You have Won! Congratulations!")
-        sys.exit()
-    elif alapok.lose(map) == True:
-        print("You have Lost!")
-        sys.exit()
-    elif alapok.vanenulla(map) == False and (alapok.vertical_move_exists(map) == True or alapok.horizontal_move_exists(map) == True):
-        alapok.mapprint(map)
-        print("erre nem tudsz már lépni de másik irányba igen")
-        scoring.writescore(map, scoring.score(map))
-    else:
-        alapok.give_new_2(map)
-        alapok.mapprint(map)
-        scoring.writescore(map, scoring.score(map))
-    return map
-
-def save(score:int,map:list) -> None:
-    """The function that saves the score and map
-
-    Args:
-        score (int): The score u want to save
-        map (list): The map u want to save
-    """
-    score = scoring.score(map)
-    alapok.mentes(map, score)
-
-def badcommand() -> str:
+def bad_command() -> str:
     """If you entered a bad command and error message will pop up
 
     Returns:
-        str: An error mesage
+        str: An error message
     """
     print("Rossz gomb")
+
+
 
 def basics(command:str,map:list) -> list:
     """This will handle every single command and what happens after that
@@ -152,24 +118,29 @@ def basics(command:str,map:list) -> list:
         list: The changed map
     """
     if command == "w":
-        map = up(map)
+        map = alapok.move_up(map)
+        get_current_state(map)
     elif command == "s":
-        map = down(map)
+        map = alapok.move_down(map)
+        get_current_state(map)
     elif command == "a":
-        map = left(map)
+        map = alapok.move_left(map)
+        get_current_state(map)
     elif command == "d":
-        map = right(map)
+        map = alapok.move_right(map)
+        get_current_state(map)
     elif command == "save":
-        score = scoring.score(map)
-        save(score,map)
+        score = alapok.score(map)
+        save(map,score)
     else:
-        badcommand()
+        bad_command()
     return map
 
 def main() -> None:
     """Generates the map, and also handles the basic function
     """
     map = start()
+    
 
     while(True):
         command = alapok.getcommand()
